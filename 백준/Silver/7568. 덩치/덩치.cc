453366a14590511cfa2fbba604d2e67d5de4ab7a
Ganghee Lee <gh_5226@naver.com>
2023-03-14 16:26:52 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -3,30 +3,6 @@
 
 using namespace std;
 
-void compare(vector<vector<int>> &a, int N) {
-    
-    int w = 0, h = 0;
-    int grade[N] = {0,};
-    
-    for(int i = 0; i < N; i++) {
-        int temp = 1;
-        w = a[i][0];
-        h = a[i][1];
-        for(int j = 0; j < N; j++) {
-            if(w < a[j][0] && h < a[j][1]) {
-                temp += 1;
-            }
-        }
-        grade[i] = temp;
-    }
-    
-    int k = 0;
-    while(grade[k]) {
-        cout << grade[k] + " ";
-        k++;
-    }
-}
-
 int main()
 {
     int N;
@@ -54,7 +30,7 @@ int main()
         grade.push_back(temp);
     }
     
-    for(int i = 0; i < N; i++) {
+    for(int i = 0; i < grade.size(); i++) {
         cout << grade[i] << " ";
     }
 
