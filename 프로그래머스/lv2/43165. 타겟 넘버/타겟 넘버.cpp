fed4301634ca652a48252572e22ac6db22127915
Ganghee Lee <gh_5226@naver.com>
2023-07-04 18:31:56 +0900

    [level 2] Title: 타겟 넘버, Time: 0.61 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,33 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int delta[2] = {1, -1};
+int answer = 0;
 
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    queue<int> oldq;
-    queue<int> newq;
-    int cur, nxt;
-    oldq.push(0);
-    for(int n : numbers) {
-        while(!oldq.empty()) {
-            cur = oldq.front(); oldq.pop();
-            for(int i = 0; i < 2; i++) {
-                nxt = cur + n * delta[i];
-                newq.push(nxt);
-            }
-        }
-        while(!newq.empty()) {
-            cur = newq.front(); newq.pop();
-            oldq.push(cur);
-        }
-    }
-    
-    while(!oldq.empty()) {
-        cur = oldq.front(); oldq.pop();
-        if(cur == target) {
+void dfs(vector<int> numbers, int target, int depth, int cur) {
+    if(depth == numbers.size()) {
+        if(target == cur) {
             answer++;
         }
+        return;
     }
+    dfs(numbers, target, depth + 1, cur + numbers[depth]);
+    dfs(numbers, target, depth + 1, cur - numbers[depth]);
+}
+
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, 0);
+    
     return answer;
 }
