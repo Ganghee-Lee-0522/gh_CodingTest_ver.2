c82bd25358678b374d1603296860fa29ef071efd
Ganghee Lee <gh_5226@naver.com>
2023-07-21 13:43:47 +0900

    [level 2] Title: 땅따먹기, Time: 6.51 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int board[100005][5];
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    
+    board[1][1] = land[0][0];
+    board[1][2] = land[0][1];
+    board[1][3] = land[0][2];
+    board[1][4] = land[0][3];
+
+    for(int i = 1; i <= land.size(); i++) {
+        board[i][1] = max(board[i - 1][2], board[i - 1][3]) + land[i - 1][0];
+        board[i][1] = max(board[i][1], board[i - 1][4] + land[i - 1][0]);
+        
+        board[i][2] = max(board[i - 1][1], board[i - 1][3]) + land[i - 1][1];
+        board[i][2] = max(board[i][2], board[i - 1][4] + land[i - 1][1]);
+        
+        board[i][3] = max(board[i - 1][1], board[i - 1][2]) + land[i - 1][2];
+        board[i][3] = max(board[i][3], board[i - 1][4] + land[i - 1][2]);
+        
+        board[i][4] = max(board[i - 1][1], board[i - 1][2]) + land[i - 1][3];
+        board[i][4] = max(board[i][4], board[i - 1][3] + land[i - 1][3]);
+    }
+    
+    answer = max(board[land.size()][1], board[land.size()][2]);
+    answer = max(answer, board[land.size()][3]);
+    answer = max(answer, board[land.size()][4]);
+
+    return answer;
+}
