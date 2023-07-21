3955e8beb3dc374e74952c852185a4d93b8416de
Ganghee Lee <gh_5226@naver.com>
2023-07-21 13:23:33 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.02 ms, Memory: 3.72 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -4,25 +4,27 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
 	
-	int ans = 0;
-	int n = board.size();
+	int ans = 0; // 사라진 인형 개수 count
+	int n = board.size(); // 격자 높이
 	stack<int> s;
 
+	// 크레인 작동
 	for (int e : moves) {
-		int pos = e - 1;
+		int pos = e - 1; // 크레인 작동 위치
 
-		for (int i = 0; i < n; i++) {
-			if (board[i][pos] == 0)
+		for (int i = 0; i < n; i++) { // 행(작을수록 높은 위치)
+			if (board[i][pos] == 0) // 해당 행에 인형 X -> 아래로!
 				continue;
 			
+			// 인형 잡기
 			int doll = board[i][pos];
 			board[i][pos] = 0;
 
 			if (!s.empty() && s.top() == doll) {
-				s.pop();
-				ans += 2;
+				s.pop(); // stack에서 인형 팡!
+				ans += 2; // 쌍으로 터져야 하니까 2개씩 count
 			}
-			else {
+			else { // 다른 종류의 인형은 stack에 적재
 				s.push(doll);
 			}
 			break;
